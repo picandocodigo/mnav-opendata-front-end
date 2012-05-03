4aec40092634fab1be7ff4a026117e3aff5d3979
Fernando Briano <fernando@picandocodigo.net>
2012-05-03 18:17:58 -0300

    Adds some content pages. Adds contact form.

--- index.rb
+++ index.rb
@@ -15,6 +15,10 @@ get '/' do
   haml :index
 end
 
+get '/about' do
+  haml :about
+end
+
 get '/artist/:id' do
   service = ArtistService.new
   @artist = service.get_artist(params[:id])
@@ -47,4 +51,40 @@ end
 post '/search_wikipedia' do
   @results = WikipediaService.search(params[:name])
   haml :wikipedia, :layout => :layout
+end
+
+
+get '/contact' do
+  haml :contact, :layout => :layout
+end
+
+# Contact form
+post '/contact' do 
+  require 'pony'
+  
+  Pony.mail(
+    # Configured for Heroku here:
+    :name => params[:name],
+    :mail => params[:mail],
+    :body => params[:body],
+    # Change contact e-mail here:
+    :to => 'someone@somewhere.com',
+    :subject => params[:name] + ' ' + params[:mail] + " has contacted you",
+    :via => :smtp,
+    :via_options => {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :domain => 'heroku.com',
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+  )
+  
+  redirect '/success' 
+end
+
+get '/success' do
+  haml :success, :layout => :layout
 end
