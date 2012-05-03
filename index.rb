ecd4d88a9fcd29e5f8cb8897b0fff3bf5daf2818
Fernando Briano <fernando@picandocodigo.net>
2012-05-03 18:51:21 -0300

    Removed contact form, changed it for static page

--- index.rb
+++ index.rb
@@ -19,16 +19,24 @@ get '/about' do
   haml :about
 end
 
+get '/contact' do
+  haml :contact
+end
+
+get '/open-data' do
+  haml :open_data
+end
+
 get '/artist/:id' do
   service = ArtistService.new
   @artist = service.get_artist(params[:id])
-  haml :artist, :layout => :layout
+  haml :artist
 end
 
 get '/artists/:name' do
   service = ArtistService.new
   @artists = service.get_artists_by_name(URI.unescape(params[:name]))
-  haml :artists, :layout => :layout
+  haml :artists
 end
 
 post '/artists' do
@@ -41,7 +49,7 @@ end
 get '/artworks/technique/:technique' do
   service = ArtworkService.new
   @artworks = service.get_artwork_by_technique(URI.unescape(params[:technique]))
-  haml :artworks, :layout => :layout
+  haml :artworks
 end
 
 post '/artworks/technique' do
@@ -50,41 +58,5 @@ end
 
 post '/search_wikipedia' do
   @results = WikipediaService.search(params[:name])
-  haml :wikipedia, :layout => :layout
-end
-
-
-get '/contact' do
-  haml :contact, :layout => :layout
-end
-
-# Contact form
-post '/contact' do
-  require 'pony'
-
-  Pony.mail(
-    # Configured for Heroku here:
-    :name => params[:name],
-    :mail => params[:mail],
-    :body => params[:body],
-    # Change contact e-mail here:
-    :to => 'someone@somewhere.com',
-    :subject => params[:name] + ' ' + params[:mail] + " has contacted you",
-    :via => :smtp,
-    :via_options => {
-      :address => 'smtp.sendgrid.net',
-      :port => '587',
-      :domain => 'heroku.com',
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
-      :authentication => :plain,
-      :enable_starttls_auto => true
-    }
-  )
-
-  redirect '/success'
-end
-
-get '/success' do
-  haml :success, :layout => :layout
+  haml :wikipedia
 end
