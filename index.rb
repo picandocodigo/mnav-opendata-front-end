5331bbeeaa3eec321207f9fb6aee1ae169e9ba13
Fernando Briano <fernando@picandocodigo.net>
2012-05-04 17:28:48 -0300

    Changes routes to Spanish

--- index.rb
+++ index.rb
@@ -15,54 +15,58 @@ get '/' do
   haml :index
 end
 
-get '/about' do
+get '/acerca-de' do
   haml :about
 end
 
-get '/contact' do
+get '/contacto' do
   haml :contact
 end
 
-get '/open-data' do
+get '/datos-abiertos' do
   haml :open_data
 end
 
-get '/artist/:id' do
+get '/artistas' do
+
+end
+
+get '/artista/:id' do
   service = ArtistService.new
   @artist = service.get_artist(params[:id])
   haml :artist
 end
 
-get '/artists/:name' do
+get '/artistas/:name' do
   service = ArtistService.new
   @artists = service.get_artists_by_name(URI.unescape(params[:name]))
   haml :artists
 end
 
-post '/artists' do
+post '/artistas' do
   if params[:name]
     name = URI.escape(params[:name])
-    redirect "/artists/#{name}"
+    redirect "/artistas/#{name}"
   end
 end
 
-get '/artworks/:id' do
+get '/obras/:id' do
   service = ArtworkService.new
   @artwork = service.get_artwork(params[:id])
   haml :artwork
 end
 
-get '/artworks/technique/:technique' do
+get '/obras/tecnica/:technique' do
   service = ArtworkService.new
   @artworks = service.get_artwork_by_technique(URI.unescape(params[:technique]))
   haml :artworks
 end
 
-post '/artworks/technique' do
-  redirect "/artworks/technique/#{URI.escape(params[:technique])}"
+post '/obras/tecnica' do
+  redirect "/obras/tecnica/#{URI.escape(params[:technique])}"
 end
 
-post '/search_wikipedia' do
+post '/buscar_wikipedia' do
   @results = WikipediaService.search(params[:name])
   haml :wikipedia
 end
