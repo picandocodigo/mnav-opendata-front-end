2b1462973a3ab73d31352dbe508e200775e0529b
Fernando Briano <fernando@picandocodigo.net>
2012-05-10 15:03:08 -0300

    Add dynamic page title, Fixes #13 Extracts helper logic into helper.rb

---
+++ helpers.rb
@@ -0,0 +1,42 @@
+helpers do
+  def show_artist_title(artist)
+    if artist['artworks_count'].to_i > 0 || artist['biography']
+      "<a href=\"/artista/#{artist['id']}\" title=\"artist['display_name']\">
+      #{artist['display_name']}</a>"
+    else
+      "#{artist['display_name']}"
+    end
+  end
+
+  def show_artist_birth(artist)
+    if artist['birth']
+      "Nacimiento: #{artist['birth']}"
+    end
+  end
+
+  def show_artist_info(artist)
+    artist_info = ''
+
+    if artist['year']
+      artist_info += "Nacimiento: #{artwork['year']}"
+    end
+
+    if artist['biography']
+      artist_info += "Biograf&iacute;a: #{artist['biography']}"
+    end
+
+    if artist['artworks']
+      artist_info += "<h3>Obras</h3>\n\t<ul>"
+      @artist['artworks'].each do |artwork|
+        artist_info += "<li><a href=\"/obras/#{artwork['id']}\" title=\"#{artwork['title']}\">
+                        #{artwork['title']}<img src=\"#{artwork['image_thumbnail_url']}\"></a></li>"
+      end
+      artist_info += "</ul>"
+    end
+    artist_info
+  end
+
+  def title
+   @title
+  end
+end
--- index.rb
+++ index.rb
@@ -4,7 +4,7 @@ require 'sass'
 require './lib/artist_service.rb'
 require './lib/artwork_service.rb'
 require './lib/wikipedia_service.rb'
-
+require './helpers.rb'
 
 get '/style.css' do
   content_type 'text/css', :charset => 'utf-8'
@@ -12,67 +12,34 @@ get '/style.css' do
 end
 
 get '/' do
+  @title = "Inicio"
   haml :index
 end
 
 get '/acerca-de' do
+  @title = "Acerca de"
   haml :about
 end
 
 get '/contacto' do
+  @title = "Contacto"
   haml :contact
 end
 
 get '/datos-abiertos' do
+  @title = "Datos abiertos"
   haml :open_data
-end
-
-helpers do
-  def show_artist_title(artist)
-    if artist['artworks_count'].to_i > 0 || artist['biography']
-      "<a href=\"/artista/#{artist['id']}\" title=\"artist['display_name']\">
-      #{artist['display_name']}</a>"
-    else
-      "#{artist['display_name']}"
-    end
-  end
-
-  def show_artist_birth(artist)
-    if artist['birth']
-      "Nacimiento: #{artist['birth']}"
-    end
-  end
-
-  def show_artist_info(artist)
-    artist_info = ''
-
-    if artist['year']
-      artist_info += "Nacimiento: #{artwork['year']}"
-    end
-
-    if artist['biography']
-      artist_info += "Biograf&iacute;a: #{artist['biography']}"
-    end
-
-    if artist['artworks']
-      artist_info += "<h3>Obras</h3>\n\t<ul>"
-      @artist['artworks'].each do |artwork|
-        artist_info += "<li><a href=\"/obras/#{artwork['id']}\" title=\"#{artwork['title']}\">
-                        #{artwork['title']}<img src=\"#{artwork['image_thumbnail_url']}\"></a></li>"
-      end
-      artist_info += "</ul>"
-    end
-    artist_info
-  end
 end
 
 get '/artista/:id' do
   service = ArtistService.new
   @artist = service.get_artist(params[:id])
+  @title = @artist['display_name']
   haml :artist
 end
 
 get '/artistas/:name' do
+  @title = "B&uacute;squeda de artistas: #{params[:name]}"
   service = ArtistService.new
   @artists = service.get_artists_by_name(URI.unescape(params[:name]))
   haml :artists
@@ -88,10 +55,12 @@ end
 get '/obras/:id' do
   service = ArtworkService.new
   @artwork = service.get_artwork(params[:id])
+  @title = @artwork['title']
   haml :artwork
 end
 
 get '/obras/tecnica/:technique' do
+  @title = "B&uacute;squeda de obras: #{params[:technique]}"
   service = ArtworkService.new
   @artworks = service.get_artwork_by_technique(URI.unescape(params[:technique]))
   haml :artworks
@@ -102,6 +71,7 @@ post '/obras/tecnica' do
 end
 
 post '/buscar_wikipedia' do
+  @title = "Buscar en Wikipedia: #{params[:name]}"
   @results = WikipediaService.search(params[:name])
   haml :wikipedia
 end
